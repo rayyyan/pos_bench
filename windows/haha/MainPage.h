83b2f58cd4593f2e55889f19319efc8f9ebce4ce
abdou 27 <horizen27@gmail.com>
2022-04-20 06:37:07 -0700

    RN windows setup up

---
+++ windows/haha/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::haha::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::haha::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
